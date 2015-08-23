12fc5b7f674cf6e6156c36cb285be1f023fab1c0
Kath <you@example.com>
2015-08-23 13:18:13 +1000

    user model and unit tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   belongs_to :quote
 
-
+  validates :user_reference, uniqueness: true
+  validates :age, numericality: { greater_than_or_equal_to: 18, less_than_or_equal_to: 69, only_integer: true }, presence: true
+  validates :trip_days, numericality: { only_integer: true }, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,19 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
     it { is_expected.to belong_to(:quote) }
+
+    it { is_expected.to validate_presence_of(:age) }
+    it { is_expected.to validate_presence_of(:trip_days) }
+
+    it { should validate_numericality_of(:trip_days).only_integer }
+    it { should validate_numericality_of(:age).only_integer }
+
+    it { should validate_uniqueness_of(:user_reference) }
+
+    it do
+      should validate_numericality_of(:age).
+        is_greater_than_or_equal_to(18).
+        is_less_than_or_equal_to(69)
+    end
+
 end
