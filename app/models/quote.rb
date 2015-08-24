d4443bd90c4c70610792a0607aa46d5b18aa68db
Kath <you@example.com>
2015-08-24 13:38:07 +1000

    add quote views

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,7 +2,16 @@ class Quote < ActiveRecord::Base
 
   belongs_to :policy
 
-  validates :age, numericality: { greater_than_or_equal_to: 18, less_than_or_equal_to: 69, only_integer: true }, presence: true
-  validates :trip_days, numericality: { only_integer: true }, presence: true
+  validates :age,
+              numericality: {
+                greater_than_or_equal_to: 18,
+                less_than_or_equal_to: 69,
+                only_integer: true },
+              presence: true
+
+  validates :trip_days,
+              numericality: {
+                only_integer: true },
+              presence: true
 
 end
